32ff22b3418f7c6c006bdd8d6209c7019ac969a9
bill <vassileiosmarkos@gmail.com>
2026-02-18 10:04:36 +0200

    Add board string

--- edax/edax-reversi-4.6/src/agent_main.c
+++ edax/edax-reversi-4.6/src/agent_main.c
@@ -2,7 +2,7 @@
 #include "move.h"
 
 int main(void) {
-    char* board_str = ""; // TODO: Add a sample board
+    const char* board_str = "(;GM[10]FF[4]SZ[8]GN[Example Game]PB[Black]PW[White];B[f5];W[d6];B[c4];W[e3];B[f4];W[c5];B[c6];W[d3])";
     int depth = 13;
     Agent* agent = agent_create(board_str, depth);
     Move* move = agent_get_best_move(agent);
